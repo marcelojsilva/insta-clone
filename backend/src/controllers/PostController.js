6646994bc238ef50c321576e8c686174aa4ee12a
marcelojsilva <marcelojosedasilva@gmail.com>
2019-06-15 11:35:22 -0300

    Front finalizado

--- backend/src/controllers/PostController.js
+++ backend/src/controllers/PostController.js
@@ -18,7 +18,7 @@ module.exports = {
         const fileName = `${name}.jpg`;
 
         await sharp(req.file.path)
-            .resize(100)
+            .resize(200)
             .jpeg({ quality: 70})
             .toFile(
                 path.resolve(req.file.destination, 'resized', fileName)
